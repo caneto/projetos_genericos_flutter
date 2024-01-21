df6f0adf83cf11c79c01afcd2c4834f9001edfd2
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2024-01-21 12:33:03 -0300

    Projeto de Task Manager, proximo multiidioma

---
+++ task_manager/android/app/src/main/kotlin/br/com/capsistema/task_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.capsistema.task_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ task_manager/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:task_manager_app/src/routes/app_router.dart';
+import 'package:task_manager_app/src/routes/pages.dart';
+import 'package:task_manager_app/src/utils/color_palette.dart';
+
+import 'src/pages/bloc_state_observer.dart';
+import 'src/tasks/data/local/data_sources/tasks_data_provider.dart';
+import 'src/tasks/data/repository/task_repository.dart';
+import 'src/tasks/presentation/bloc/tasks_bloc.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = BlocStateOberver();
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  runApp(MyApp(
+    preferences: preferences,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  final SharedPreferences preferences;
+
+  const MyApp({super.key, required this.preferences});
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) =>
+          TaskRepository(taskDataProvider: TaskDataProvider(preferences)),
+      child: BlocProvider(
+        create: (context) => TasksBloc(context.read<TaskRepository>()),
+        child: MaterialApp(
+          title: 'Task Manager',
+          debugShowCheckedModeBanner: false,
+          initialRoute: Pages.initial,
+          onGenerateRoute: onGenerateRoute,
+          theme: ThemeData(
+            fontFamily: 'Sora',
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            canvasColor: Colors.transparent,
+            colorScheme: ColorScheme.fromSeed(seedColor: kPrimaryColor),
+            useMaterial3: true,
+          ),
+        ),
+      ),
+    );
+  }
+}
